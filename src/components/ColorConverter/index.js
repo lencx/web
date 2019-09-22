7c15483196eecadd149194952dede4a35ee9512c
lencx <cxin1314@gmail.com>
2019-09-22 16:05:42 +0800

    chore: color reset

--- src/components/ColorConverter/index.js
+++ src/components/ColorConverter/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useRef } from 'react'
 import copy from 'copy-to-clipboard'
 import { ToastContainer, toast } from 'react-toastify'
+// import ConverterIcon from '~components/icon/ConverterIcon'
+import CloseIcon from '~components/icon/CloseIcon'
 import * as c from './fns'
 
 import './index.scss'
@@ -23,15 +25,17 @@ const fns  = [
   'nameToHSL',
 ]
 
+// const formatStr = str => str.match(/(.*)(To)(.*)/)
+
 export default () => {
-  const [value, setValue] = useState(null)
+  const [value, setValue] = useState('')
   const [Fn, setFn] = useState('hexToRGB')
   const [converterValue, setConverter] = useState('')
   const handleConverter = () => {
     if (!value) return setConverter('Please enter the color value you want to convert')
-    // fix: spaces cause invalid colors
+    // fix: space cause invalid colors
     const val = value.replace(/\s/g, '')
-    console.log(val)
+    console.log(val, c, Fn)
     setConverter(c[Fn](val))
   }
   const handleInput = e => {
@@ -63,13 +67,19 @@ export default () => {
     <div className="n__card converter">
       <h3>Color</h3>
       <ul>
-        <li>Click the `converter` button or press Enter to start the conversion.</li>
-        <li>Click on the output color to copy to the clipboard</li>
+        <li>name(colors-names): `red`, `green`, ...</li>
+        <li>click the `converter` button or press Enter to start the conversion.</li>
+        <li>click on the output color to copy to the clipboard.</li>
       </ul>
       <select defaultValue="hexToRGB" onChange={e => setFn(e.target.value)}>
-        {fns.map(item => <option key={item}>{item}</option>)}
+        {fns.map(item => {
+          return <option key={item} value={item}>{item.replace('To', ' => ')}</option>
+        })}
       </select>
-      <input type="text" onChange={handleInput} onKeyPress={handleInput} />
+      <div className="color-input">
+        <input type="text" onChange={handleInput} value={value} onKeyPress={handleInput} />
+        {value && <button className="reset-btn btn" onClick={() => setValue('')}><CloseIcon /></button>}
+      </div>
       <button className="btn" onClick={handleConverter}>Converter</button>
       <p className="output"><b>Output:</b> {converterValue ? renderOutput : '⌛️Waiting for input...'}</p>
       <ToastContainer />
