ac9a385914fa0d1459f08596a0883a846fcba660
lencx <cxin1314@gmail.com>
2020-04-04 16:29:25 +0800

    chore: style

--- src/pages/tools/converter.tsx
+++ src/pages/tools/converter.tsx
@@ -6,10 +6,12 @@
 import React from 'react';
 import Layout from '~layout/post';
 import ConverterColor from '~comps/converter_color';
+import SEO from '~common/seo';
 
 export default function ConverterTools() {
   return (
     <Layout>
+      <SEO title="Converter Tools" />
       <h2>Converter Tools</h2>
       <ConverterColor />
     </Layout>
